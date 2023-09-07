767602657d4749f0fc8b3c5c28a949e78b8c655a
Daniel Trujillo <danielfetrujillo@gmail.com>
2023-09-07 12:10:15 -0500

    cleanup

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>App</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
