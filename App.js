f7047b277be6213d719bd1ac1962ef68f75eefb8
Daniel Trujillo <danielfetrujillo@gmail.com>
2023-09-07 14:29:54 -0500

    implemented basic navigation

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {Home, Welcome} from './src/screens';
 
+const Stack = createNativeStackNavigator();
+
 const App = () => {
   return (
-    <>
-      <Welcome />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
-const Welcome = () => {
+const Welcome = ({navigation}) => {
   return (
     <View>
       <Text>Welcome</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
     </View>
   );
 };
