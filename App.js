02a6d1ca1fed3d65a35588416c3f241bcb844b3a
Daniel Trujillo <danielfetrujillo@gmail.com>
2023-09-18 11:33:57 -0500

    created screen template

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {useTranslation} from 'react-i18next';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import './i18n.config';
 import {Home, Welcome, Register, Recovery, Settings} from './src/screens';
@@ -16,56 +17,58 @@ const App = () => {
   const firstUse = true;
 
   return (
-    <NavigationContainer>
-      {firstUse ? (
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Welcome"
-            component={Welcome}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Register"
-            component={Register}
-            options={{title: t('navigation.accountsetupflow.register')}}
-          />
-          <Stack.Screen
-            name="Recovery"
-            component={Recovery}
-            options={{title: t('navigation.accountsetupflow.recovery')}}
-          />
-        </Stack.Navigator>
-      ) : (
-        <Tab.Navigator
-          initialRouteName="Home"
-          screenOptions={{
-            headerShown: false,
-          }}>
-          <Tab.Screen
-            name="Home"
-            component={Home}
-            options={{
-              tabBarLabel: t('navigation.mainflow.home'),
-              // eslint-disable-next-line react/no-unstable-nested-components
-              tabBarIcon: ({color, size}) => (
-                <Icon name="wallet" color={color} size={size} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Settings"
-            component={Settings}
-            options={{
-              tabBarLabel: t('navigation.mainflow.settings'),
-              // eslint-disable-next-line react/no-unstable-nested-components
-              tabBarIcon: ({color, size}) => (
-                <Icon name="cog" color={color} size={size} />
-              ),
-            }}
-          />
-        </Tab.Navigator>
-      )}
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        {firstUse ? (
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Welcome"
+              component={Welcome}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Register"
+              component={Register}
+              options={{title: t('navigation.accountsetupflow.register')}}
+            />
+            <Stack.Screen
+              name="Recovery"
+              component={Recovery}
+              options={{title: t('navigation.accountsetupflow.recovery')}}
+            />
+          </Stack.Navigator>
+        ) : (
+          <Tab.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+              headerShown: false,
+            }}>
+            <Tab.Screen
+              name="Home"
+              component={Home}
+              options={{
+                tabBarLabel: t('navigation.mainflow.home'),
+                // eslint-disable-next-line react/no-unstable-nested-components
+                tabBarIcon: ({color, size}) => (
+                  <Icon name="wallet" color={color} size={size} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Settings"
+              component={Settings}
+              options={{
+                tabBarLabel: t('navigation.mainflow.settings'),
+                // eslint-disable-next-line react/no-unstable-nested-components
+                tabBarIcon: ({color, size}) => (
+                  <Icon name="cog" color={color} size={size} />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        )}
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
---
+++ src/atoms/ScreenTemplate.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+
+const ScreenTemplate = ({children}) => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        // Paddings to handle safe area
+        paddingTop: insets.top + 10,
+        paddingBottom: insets.bottom + 10,
+        paddingLeft: insets.left + 20,
+        paddingRight: insets.right + 20,
+        backgroundColor: 'white',
+      }}>
+      {children}
+    </View>
+  );
+};
+
+export {ScreenTemplate};
