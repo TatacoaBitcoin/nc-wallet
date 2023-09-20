e290fd0e60d5b0633ddae084c30eac79a44f1bc3
Daniel Trujillo <danielfetrujillo@gmail.com>
2023-09-20 10:37:44 -0500

    added reset button

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
+
+import {useAccountState} from '../context/account.provider';
 
 const Settings = () => {
+  const {resetAccount} = useAccountState();
+
   return (
     <View>
       <Text>Settings</Text>
+      <Button onPress={() => resetAccount()} title="Reset" />
     </View>
   );
 };
