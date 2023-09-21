31968c1b1c1cf456498f1e714dcf75c10e5b31b6
2624789 <2624789@users.noreply.github.com>
2023-09-21 14:22:34 -0500

    Adds breez sdk

--- src/hooks/useAccount.js
+++ src/hooks/useAccount.js
@@ -1,19 +1,37 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+
+import {getBalance, initNode} from '../breez';
 
 export const useAccount = () => {
   const [account, setAccount] = useState(null);
+  const [balance, setBalance] = useState({
+    lightning: null,
+    btc: null,
+  });
+
+  useEffect(() => {
+    if(!account) return;
+
+    async function fetchBalance() {
+      const nodeBalance = await getBalance();
+      setBalance(nodeBalance);
+    };
+
+    fetchBalance();
+  }, [account]);
 
   //TODO: store seed words safely using encrypted storage
 
-  const saveAccount = words => {
-    setAccount(words);
+  const saveAccount = async words => {
+    const nodeIsOk = await initNode(words);
+    if (nodeIsOk) setAccount(words);
   };
 
   const loadAccount = () => {
     // retrieves seed words
   };
 
-  const resetAccount = () => setAccount(null);
+  const resetAccount = async() => setAccount(null);
 
-  return {account, saveAccount, loadAccount, resetAccount};
+  return {account, balance, saveAccount, loadAccount, resetAccount};
 };
