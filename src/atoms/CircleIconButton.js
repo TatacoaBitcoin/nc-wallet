c7ad1614e04eccc9be5fb14b08b63599fb8a5354
2624789 <2624789@users.noreply.github.com>
2023-09-28 20:34:20 -0500

    Home screen style (#9)
    * CircleIconButton component
    
    * Update colors
    
    * Style bottom tab
    
    * adjust spacing

---
+++ src/atoms/CircleIconButton.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, Pressable, View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+const CircleIconButton = ({onPress, icon}) => (
+  <View>
+    <Pressable style={styles.circle} onPress={onPress} activeOpacity={0.8}>
+      <Icon name={icon} color="purple" size={30} />
+    </Pressable>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  circle: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'lightgrey',
+    borderColor: 'purple',
+    borderWidth: 1,
+  },
+});
+
+export {CircleIconButton};
--- src/atoms/ScreenTemplate.js
+++ src/atoms/ScreenTemplate.js
@@ -10,11 +10,11 @@ const ScreenTemplate = ({children}) => {
       style={{
         flex: 1,
         // Paddings to handle safe area
-        paddingTop: insets.top + 10,
-        paddingBottom: insets.bottom + 10,
-        paddingLeft: insets.left + 20,
-        paddingRight: insets.right + 20,
-        backgroundColor: 'gray',
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left,
+        paddingRight: insets.right,
+        backgroundColor: 'lightgray',
       }}>
       {children}
     </View>
