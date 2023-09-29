8e53f3f0b0cd142b3275c6cad84f57f08d546655
Daniel Trujillo <danielfetrujillo@gmail.com>
2023-09-29 18:52:28 -0500

    creates qr on user input

--- src/screens/Receive.js
+++ src/screens/Receive.js
@@ -1,19 +1,21 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput, Button} from 'react-native';
+import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
 import {receivePayment} from '@breeztech/react-native-breez-sdk';
+import QRCode from 'react-native-qrcode-svg';
 
 import {useLoading} from '../hooks/useLoading';
 
 const Receive = () => {
   const [isLoading, withLoading] = useLoading();
   const [invoice, setInvoice] = useState();
+  const [amount, setAmount] = useState('');
 
-  const getInvoice = amount => {
+  const getInvoice = sats => {
     withLoading(async () => {
       try {
         const response = await receivePayment({
-          amountSats: amount,
-          description: `Invoice for ${amount} sats`,
+          amountSats: sats,
+          description: `Invoice for ${sats} sats`,
         });
         setInvoice(response['lnInvoice']['bolt11']);
       } catch (error) {
@@ -24,11 +26,36 @@ const Receive = () => {
 
   return (
     <View>
-      <Text>Enter amount</Text>
-      <TextInput />
-      <Button title="Generate QR Code" onPress={() => getInvoice(100)} />
+      <Text style={styles.text}>Enter amount</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="0"
+        placeholderTextColor={'black'}
+        inputMode="numeric"
+        value={amount}
+        onChangeText={setAmount}
+      />
+      <Button
+        title="Generate QR Code"
+        onPress={() => getInvoice(Number(amount))}
+        disabled={!amount}
+      />
+      {isLoading && <Text style={styles.text}>Creating invoice ...</Text>}
+      {invoice && (
+        <QRCode value={invoice} size={250} backgroundColor="transparent" />
+      )}
     </View>
   );
 };
 
 export {Receive};
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+  },
+  input: {
+    color: 'black',
+    marginBottom: 20,
+  },
+});
