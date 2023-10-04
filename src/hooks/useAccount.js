5820c4be47a4cb84a52954a147df3d7820b78999
2624789 <2624789@users.noreply.github.com>
2023-10-04 18:10:11 -0500

    Auth (#12)
    * Secure persist account

--- src/hooks/useAccount.js
+++ src/hooks/useAccount.js
@@ -1,6 +1,31 @@
 import {useEffect, useState} from 'react';
+import * as Keychain from 'react-native-keychain';
+import {mnemonicToSeedHex, entropyToMnemonic, Wordlists} from '@dreson4/react-native-quick-bip39';
 
 import {getBalance, initNode} from '../breez';
+
+const secureStore = async words => {
+  const seed = mnemonicToSeedHex(words);
+  await Keychain.setGenericPassword("seed", seed);
+};
+
+const secureRetrieve = async () => {
+  try {
+    const credentials = await Keychain.getGenericPassword();
+    if (credentials) {
+      return entropyToMnemonic(credentials.password, Wordlists['en']);
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error("Keychain couldn't be accessed!", error);
+    return false;
+  }
+};
+
+const secureReset = async () => {
+  return await Keychain.resetGenericPassword();
+};
 
 export const useAccount = () => {
   const [account, setAccount] = useState(null);
@@ -8,6 +33,10 @@ export const useAccount = () => {
     lightning: null,
     btc: null,
   });
+
+  useEffect(() => {
+    loadAccount();
+  }, []);
 
   useEffect(() => {
     if (!account) return;
@@ -20,18 +49,31 @@ export const useAccount = () => {
     fetchBalance();
   }, [account]);
 
-  //TODO: store seed words safely using encrypted storage
-
   const saveAccount = async words => {
     const nodeIsOk = await initNode(words);
-    if (nodeIsOk) setAccount(words);
+    if (nodeIsOk) {
+      await secureStore(words);
+      setAccount(true);
+    };
   };
 
-  const loadAccount = () => {
-    // retrieves seed words
+  const loadAccount = async () => {
+    const words = await secureRetrieve();
+    if (!words) {
+      setAccount(null)
+      return;
+    }
+
+    const nodeIsOk = await initNode(words);
+    if (nodeIsOk) {
+      setAccount(true);
+    };
   };
 
-  const resetAccount = async () => setAccount(null);
+  const resetAccount = async () => {
+    await secureReset();
+    setAccount(null);
+  };
 
   return {account, balance, saveAccount, loadAccount, resetAccount};
 };
