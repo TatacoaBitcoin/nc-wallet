a6893e45abed76c9101ab0e6c5223c6c80ada377
2624789 <2624789@users.noreply.github.com>
2023-10-04 17:54:10 -0500

    Secure persist account

--- src/hooks/useAccount.js
+++ src/hooks/useAccount.js
@@ -1,6 +1,29 @@
 import {useEffect, useState} from 'react';
+import * as Keychain from 'react-native-keychain';
+import {mnemonicToSeedHex, entropyToMnemonic, Wordlists} from '@dreson4/react-native-quick-bip39';
 
 import {getBalance, getNodeConfig, getSeed, initNode} from '../breez';
+
+const secureStore = async words => {
+  const seed = mnemonicToSeedHex(words);
+  await Keychain.setGenericPassword("seed", seed);
+};
+
+const secureRetrieve = async () => {
+  try {
+    const credentials = await Keychain.getGenericPassword();
+    if (credentials) {
+      return entropyToMnemonic(credentials.password, Wordlists['en']);
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error("Keychain couldn't be accessed!", error);
+    return false;
+  }
+};
+
+// await Keychain.resetGenericPassword();
 
 export const useAccount = () => {
   const [account, setAccount] = useState(null);
@@ -8,6 +31,10 @@ export const useAccount = () => {
     lightning: null,
     btc: null,
   });
+
+  useEffect(() => {
+    loadAccount();
+  }, []);
 
   useEffect(() => {
     if (!account) return;
@@ -20,16 +47,29 @@ export const useAccount = () => {
     fetchBalance();
   }, [account]);
 
-  //TODO: store seed words safely using encrypted storage
   const saveAccount = async words => {
+    const seed = await getSeed(words);    
+    const config = await getNodeConfig();
+    const nodeIsOk = await initNode(config, seed);
+    if (nodeIsOk) {
+      await secureStore(words);
+      setAccount(true);
+    };
+  };
+
+  const loadAccount = async () => {
+    const words = secureRetrieve();
+    if (!words) {
+      setAccount(null)
+      return;
+    }
+
     const seed = await getSeed(words);
     const config = await getNodeConfig();
     const nodeIsOk = await initNode(config, seed);
-    if (nodeIsOk) setAccount(words);
-  };
-
-  const loadAccount = () => {
-    // retrieves seed words
+    if (nodeIsOk) {
+      setAccount(true);
+    };
   };
 
   const resetAccount = async () => setAccount(null);
