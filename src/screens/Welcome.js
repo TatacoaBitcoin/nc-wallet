683b2751ea17594f204eaaffd2cbcfc6942fe0e3
2624789 <2624789@users.noreply.github.com>
2023-10-12 11:00:25 -0500

    Welcome screen linear background and logo

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,25 +1,60 @@
 import React from 'react';
-import {Text, Button} from 'react-native';
+import {Image, StyleSheet, Text, View, Button} from 'react-native';
 import {useTranslation} from 'react-i18next';
+import LinearGradient from 'react-native-linear-gradient';
 
 import {ScreenTemplate} from '../atoms';
+import colors from '../styles/colors';
+import {margin} from '../styles/spacing';
+import Logo from '../assets/images/logo.png';
 
 const Welcome = ({navigation}) => {
   const {t} = useTranslation();
 
   return (
     <ScreenTemplate>
-      <Text>{t('welcome.title')}</Text>
-      <Button
-        title={t('welcome.btn.register')}
-        onPress={() => navigation.navigate('Register')}
-      />
-      <Button
-        title={t('welcome.btn.recovery')}
-        onPress={() => navigation.navigate('Recovery')}
-      />
+      <LinearGradient
+        colors={[colors.purple, colors.black]}
+        style={styles.container}
+      >
+        <View style={styles.logoContainer}>
+          <Image source={Logo} />
+          <Text style={styles.text}>Tatacoa Wallet</Text>
+        </View>
+        <View style={styles.buttonsContainer}>
+          <Button
+            title={t('welcome.btn.register')}
+            onPress={() => navigation.navigate('Register')}
+          />
+          <Button
+            title={t('welcome.btn.recovery')}
+            onPress={() => navigation.navigate('Recovery')}
+          />
+        </View>
+      </LinearGradient>
     </ScreenTemplate>
   );
 };
 
 export {Welcome};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 4,
+  },
+  text: {
+    color: colors.lightPurple,
+    fontFamily: 'Inter-SemiBold',
+    fontSize: 40,
+  },
+  buttonsContainer: {
+    flex: 1,
+    gap: margin.md,
+  }
+});
