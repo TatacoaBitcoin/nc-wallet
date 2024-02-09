a9f63dce6ce4343add15047de4f5d253bfdbaf34
2624789 <2624789@users.noreply.github.com>
2024-02-09 17:38:38 -0500

    Deprecates PaymentModal

--- src/molecules/index.js
+++ src/molecules/index.js
@@ -1,6 +1,5 @@
 export * from './AlertModal';
 export * from './ExitModal';
-export * from './PaymentModal';
 export * from './SeedModal';
 export * from './SuccessModal';
 export * from './WordList';
