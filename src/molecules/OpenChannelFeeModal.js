a3cfe2dc5801ade1b577493e23042c6c52739436
Daniel Trujillo <danielfetrujillo@gmail.com>
2024-03-01 10:49:06 -0500

    adds feedback for fee fetching

--- src/molecules/OpenChannelFeeModal.js
+++ src/molecules/OpenChannelFeeModal.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {StyleSheet, ScrollView} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {openChannelFee} from '@breeztech/react-native-breez-sdk';
+import {MaterialIndicator} from 'react-native-indicators';
 
 import {Modal, Text} from '../atoms';
 import {fonts, margin} from '../styles/spacing';
@@ -10,6 +11,7 @@ import {parseSats} from '../utils/parsing';
 
 const OpenChannelFeeModal = ({isVisible, onClose}) => {
   const [fee, setFee] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
   const getFee = async () => {
     try {
@@ -17,6 +19,8 @@ const OpenChannelFeeModal = ({isVisible, onClose}) => {
       setFee(response.feeParams.minMsat);
     } catch (err) {
       console.log('error fetching channel openning fee', err);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -25,15 +29,19 @@ const OpenChannelFeeModal = ({isVisible, onClose}) => {
   }, []);
 
   return (
-    <Modal isVisible={isVisible} onClose={onClose} size="sm">
+    <Modal isVisible={isVisible} onClose={onClose} size="xs">
       <ScrollView contentContainerStyle={styles.container}>
         <Icon name="information" color={Colors.yellow} size={fonts.xxxl} />
         <Text size={fonts.md} align="center">
           Channel openning fees:
         </Text>
-        <Text size={fonts.md} align="center">
-          {parseSats(fee)} sats
-        </Text>
+        {isLoading ? (
+          <MaterialIndicator color={Colors.purple} size={fonts.lg} />
+        ) : (
+          <Text size={fonts.md} align="center">
+            {parseSats(fee)} sats
+          </Text>
+        )}
       </ScrollView>
     </Modal>
   );
